772e29235b5b19facf3caec735658721ff4c305a
eliasvansickle <eliasvansickle@gmail.com>
2015-06-06 16:09:59 -0700

    "Cleaned up the html and jQuery to get rid of unecessary classes"

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 $(document).ready(function() {
-	$("img.tone").click(function(){
-		$(this).hide(this);
-	});
-	$("img.ttwo").click(function(){
-		$(this).hide(this);
-	});
-	$("img.tthree").click(function(){
-		$(this).hide(this);
-	});
-	$("img.tfour").click(function(){
-		$(this).hide(this);
-	});
-	$("img.bone").click(function(){
-		$(this).hide(this);
-	});
-	$("img.btwo").click(function(){
-		$(this).hide(this);
-	});
-	$("img.bthree").click(function(){
-		$(this).hide(this);
-	});
-	$("img.bfour").click(function(){
+	$("img").click(function(){
 		$(this).hide(this);
 	});
 	$("button").click(function() {
