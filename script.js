551914c9d98629fec218a949a241d8fc640e147a
eliasvansickle <eliasvansickle@gmail.com>
2015-06-06 14:09:15 -0700

    "Final Product looking sweet and sexy"

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+	$("img.tone").click(function(){
+		$("img.tone").hide("img.tone");
+	});
+	$("img.ttwo").click(function(){
+		$("img.ttwo").hide("img.ttwo");
+	});
+	$("img.tthree").click(function(){
+		$("img.tthree").hide("img.tthree");
+	});
+	$("img.tfour").click(function(){
+		$("img.tfour").hide("img.tfour");
+	});
+	$("img.bone").click(function(){
+		$("img.bone").hide("img.bone");
+	});
+	$("img.btwo").click(function(){
+		$("img.btwo").hide("img.btwo");
+	});
+	$("img.bthree").click(function(){
+		$("img.bthree").hide("img.bthree");
+	});
+	$("img.bfour").click(function(){
+		$("img.bfour").hide("img.bfour");
+	});
+	$("button").click(function() {
+		$("img").show();
+	});
 
 });
